763165b9ca3aa9aadf9030752b689383c471751b
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2023-08-24 00:40:44 +0200

    feat: Fixed deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,28 @@
 import { ethers } from "hardhat";
 
-// TODO: Check this deploy function. It was from a 2021 post.
-// I think Hardhat has new ways to do this
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log(
+    "Deploying the contracts with the account:",
+    await deployer.getAddress()
+  );
 
   // We get the contract to deploy
-  const RolesContract = await ethers.getContractFactory('Roles');
+  const RolesContract = await ethers.getContractFactory("Roles");
   const rolescontract = await RolesContract.deploy();
+  await rolescontract.waitForDeployment();
 
-  await rolescontract.deployed();
+  const rolesContractAddress = await rolescontract.getAddress();
 
-  console.log('RolesContract deployed to:', rolescontract.address);
+  console.log("Roles contract deployed to:", rolesContractAddress);
 
-  const NFTContract = await ethers.getContractFactory('NFT');
-  const nftcontract = await NFTContract.deploy(rolescontract.address);
+  const NFTContract = await ethers.getContractFactory("NFT");
+  const nftcontract = await NFTContract.deploy(rolesContractAddress);
+  await nftcontract.waitForDeployment();
 
-  console.log('NFT deployed to:', nftcontract.address);
+  const nftContractAddress = await nftcontract.getAddress();
+
+  console.log("NFT contract deployed to:", nftContractAddress);
 }
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.

6823d4f9aaf0fefa2351a195529ddc40a238127a
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2023-08-25 00:50:02 +0200

    feat: Add custom radio input

---
+++ frontend/src/components/Inputs/RadioInput.tsx
@@ -0,0 +1,19 @@
+import { InputHTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
+
+type RadioInputProps = InputHTMLAttributes<HTMLInputElement> & {
+  className?: string;
+};
+
+export const RadioInput = ({
+  children,
+  className,
+  ...rest
+}: RadioInputProps) => {
+  return (
+    <label>
+      <input type="radio" className={twMerge("mr-1", className)} {...rest} />
+      {children}
+    </label>
+  );
+};

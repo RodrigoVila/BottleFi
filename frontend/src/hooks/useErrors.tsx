a5fd202531286223da428e4209cedfd6fd7348a3
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2023-09-07 19:59:40 +0200

    feat: Add error for invalid token transfer

--- frontend/src/hooks/useErrors.tsx
+++ frontend/src/hooks/useErrors.tsx
@@ -1,12 +1,13 @@
 import { serializeError } from "eth-rpc-errors";
 
-import {parseRevertErrorMessage } from "@utils/parse";
+import { parseRevertErrorMessage } from "@utils/parse";
 
 import { useToastNotifications } from "./useToastNotifications";
 
 export const useErrors = () => {
   const { showErrorNotification } = useToastNotifications();
 
+  // TODO: Improve this hook and how errors are being parsed/read
   const notifyCatchErrors = (err: unknown): string | undefined => {
     const error = serializeError(err);
 
@@ -17,7 +18,12 @@ export const useErrors = () => {
 
     if (error.message.includes("reverted with reason")) {
       const err = parseRevertErrorMessage(error);
-      if (err) showErrorNotification(err);
+      if (err.includes("Invalid tokens can")) {
+        const err = "Invalid tokens can't be transferred";
+        showErrorNotification(err);
+        return;
+      }
+      showErrorNotification(err);
       return;
     }
   };

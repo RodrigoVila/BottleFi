89465f48a06335f4334b292f435bfe192b6a9110
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2023-09-08 20:51:06 +0200

    feat: Add Sepolia network to config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`${process.env.SEPOLIA_PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;

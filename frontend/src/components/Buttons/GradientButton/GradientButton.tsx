4290453fc34d3d49e61330f0b084b38c5450217e
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2023-09-10 01:02:43 +0200

    feat: Add longwait to gradient button

--- frontend/src/components/Buttons/GradientButton/GradientButton.tsx
+++ frontend/src/components/Buttons/GradientButton/GradientButton.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode, useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 import { Spinner } from "@components/Spinner";
@@ -17,13 +17,28 @@ export const GradientButton = ({
   className,
   ...rest
 }: GradientButtonProps) => {
+  const [isLongWait, setLongWait] = useState(false);
+
   const loadingStyles = loading
     ? "bg-slate-500 pointer-events-none"
     : "bg-black";
   const iconStyles = icon ? "justify-start" : "justify-center";
+
+  useEffect(() => {
+    if (!loading) return;
+
+    const timer = setTimeout(() => {
+      setLongWait(true);
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, [loading]);
+
   return (
     <button
-      className={twMerge("w-full", loading || rest.disabled ? "bg-slate-500" : "bg-gradient")}
+      className={twMerge(
+        "w-full",
+        loading || rest.disabled ? "bg-slate-500" : "bg-gradient"
+      )}
       disabled={rest.disabled || loading}
       {...rest}
     >
@@ -36,7 +51,18 @@ export const GradientButton = ({
         )}
       >
         {!loading && icon && <span>{icon}</span>}
-        {loading ? <Spinner /> : <span>{children}</span>}
+        {loading ? (
+          <div>
+            <Spinner />
+            {isLongWait ? (
+              <span className="mr-2 text-xs">
+                Sorry, sometimes transactions can be slow...
+              </span>
+            ) : null}
+          </div>
+        ) : (
+          <span>{children}</span>
+        )}
       </div>
     </button>
   );

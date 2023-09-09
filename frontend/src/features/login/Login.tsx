e5b18239fccab108fbe5ed28c2939b1d3d1dfa3b
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2023-09-09 14:38:34 +0200

    fix: Login text styles

--- frontend/src/features/login/Login.tsx
+++ frontend/src/features/login/Login.tsx
@@ -12,14 +12,14 @@ export const Login = () => {
     <>
       {user && <Navigate to="/dashboard" />}
       <div className="w-full h-screen bg-center bg-no-repeat bg-cover bg-login">
-        <div className="w-full h-full center bg-overlay">
+        <div className="w-full h-full center bg-overlay font-marcellus">
           <div className="flex-col max-w-xl gap-5 mx-2 center md:mx-0">
             <Logo />
-            <p className="text-3xl font-bold text-center font-cormorant">
+            <p className="text-4xl text-center font-cormorant">
               <span className="font-bold gradient-text">Exclusive</span>{" "}
               certificates for each produced bottle.
             </p>
-            <p className="text-2xl text-center font-lato">
+            <p className="text-3xl text-center">
               Avoid counterfeit and bring trust to your customers.
             </p>
             <div className="flex flex-col items-center justify-center gap-2">

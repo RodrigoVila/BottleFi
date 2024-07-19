73766410c138734a15419017c7fa11d9d8eb695f
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-07-19 16:53:14 +0200

    fix: Update icon and fix styles

--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -1,4 +1,4 @@
-import { GiSquareBottle } from "react-icons/gi";
+import { FaWineBottle } from "react-icons/fa";
 import { twMerge } from "tailwind-merge";
 
 type LogoProps = {
@@ -14,14 +14,14 @@ export const Logo = ({ type = "login" }: LogoProps) => {
     <div
       data-testid="logo"
       className={twMerge(
-        "flex items-center justify-center md:justify-start gap-1 flex-1",
+        "flex items-start justify-center md:justify-start gap-1 flex-1",
         containerStyles
       )}
     >
-      <GiSquareBottle
+      <FaWineBottle
         data-testid="Icon logo"
-        size={type === "login" ? 40 : 30}
-        className="text-pink-600"
+        size={type === "login" ? 40 : 25}
+        className="text-white -rotate-45"
       />
       <h1
         className={twMerge(

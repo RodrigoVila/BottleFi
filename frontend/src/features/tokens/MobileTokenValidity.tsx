2dc912dd14e1fa0035683172f94c92f130bf1a27
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-12-02 17:19:57 +0100

    fix: Mobile token styles

--- frontend/src/features/tokens/MobileTokenValidity.tsx
+++ frontend/src/features/tokens/MobileTokenValidity.tsx
@@ -1,9 +1,15 @@
+import { useThemeContext } from "@hooks";
 import { TokenValidity } from ".";
+import { twJoin, twMerge } from "tailwind-merge";
 
 export const MobileTokenValidity = () => {
+  const {isProfessionalTheme} = useThemeContext()
+
+  const themeStyles = isProfessionalTheme ? "bg-slate-900" : "bg-layout"
+
   return (
-    <div className="w-full h-screen bg-center bg-no-repeat bg-cover bg-layout">
-      <div className="h-full center">
+    <div className={twJoin("w-full h-full min-h-screen bg-center bg-no-repeat bg-cover bg-layout", themeStyles)}>
+      <div className="h-full center my-16">
         <TokenValidity />
       </div>
     </div>
